b2ce8cc30eb139a6234420008c9d2ceafdc4b34a
kishangupta511 <149975760+kishangupta511@users.noreply.github.com>
2024-10-14 12:58:43 +1100

    Reorganize files into src and data folders

---
+++ src/run.py
@@ -0,0 +1,52 @@
+import json
+from solve import solve_multi_period_NBA
+import pandas as pd
+from retrieve import get_fixtures, get_players
+from retrieve import get_team
+import os
+import sys
+import os
+import sys
+
+# Read options from the file
+with open('solver_settings.json') as f:
+    solver_options = json.load(f)
+
+def refresh_data():
+ # Attempt to add the private src folder to the path if projections.py exists
+    projections_path = os.path.join(os.path.dirname(__file__), '..', 'src', 'project.py')
+
+    if os.path.exists(projections_path):
+        # Add the private src folder to sys.path
+        sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+        projections_available = True
+        try:
+            from project import mins_projection, player_projection 
+        except ImportError:
+            projections_available = False
+            print("Could not import from project.py")
+    else:
+        projections_available = False
+        print("Public solver does not allow projections updates, please pull the latest projections from the Github before solver execution")
+    
+    projected_stats = pd.read_csv('data/rotowire-nba-projections.csv', skiprows=1)
+    get_players()
+    get_fixtures()
+    if projections_available:
+        xmins = mins_projection(projected_stats)
+        player_projection(projected_stats, xmins)
+        print('Data Successfully Updated\n')
+
+def run_optimisation():
+    # Refresh the data
+    refresh_data()
+
+    fantasy_team = get_team(solver_options.get('team_id'))
+
+    # Solve the NBA problem
+    solve_multi_period_NBA(squad=fantasy_team['initial_squad'],
+                        sell_prices=fantasy_team['sell_prices'], 
+                        gd=fantasy_team['gd'], itb=fantasy_team['itb'], options=solver_options)
+    
+if __name__ == '__main__':
+    run_optimisation()
